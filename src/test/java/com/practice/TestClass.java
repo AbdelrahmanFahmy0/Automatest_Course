a2207fad1c3e6538908f8ba79d54df6ad89729c6
Abdelrahman Fahmy <abdoofahmy222@gmail.com>
2026-06-26 15:29:41 +0300

    Add Selenium WebDriver setup with optimized Chrome options

--- src/test/java/com/practice/TestClass.java
+++ src/test/java/com/practice/TestClass.java
@@ -1,10 +1,34 @@
 package com.practice;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
 public class TestClass {
 
     @Test
     public void testCase() {
+        //hii
+        WebDriver driver = new ChromeDriver(getOptimizedOptions());
+        driver.get("https://www.google.com");
+        driver.quit();
+    }
+
+    public static ChromeOptions getOptimizedOptions() {
+        ChromeOptions options = new ChromeOptions();
+
+        // Essential execution mode
+//        options.addArguments("--headless");
+
+        // CI/CD-specific stability
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        // Performance & consistency
+        options.addArguments("--disable-extensions");
+        options.addArguments("--window-size=1920,1080");
+
+        return options;
     }
 }
